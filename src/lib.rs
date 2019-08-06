38e2d8c9aa161bb5a1ee720e280d511f3b4aaf01
Liu Jiang <gerry@linux.alibaba.com>
2019-08-07 00:46:36 +0800

    Simplify code according to rust edition 2018
    Simpify source according to rust edition 2018, and also fix simple
    compilation errors.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,21 @@
 // Copyright © 2019 Intel Corporation
 //
 // SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
-//
-
 //#![deny(missing_docs)]
+
 //! Virtual Function I/O (VFIO) API
-extern crate byteorder;
-extern crate devices;
-extern crate kvm_bindings;
-extern crate kvm_ioctls;
-extern crate log;
-extern crate pci;
-extern crate vfio_bindings;
-extern crate vm_allocator;
-extern crate vm_memory;
+
 #[macro_use]
 extern crate vmm_sys_util;
 
 mod vfio_device;
 mod vfio_ioctls;
-mod vfio_pci;
+
+pub use vfio_device::{VfioDevice, VfioError};
 
 use std::mem::size_of;
 
-pub use vfio_device::{VfioDevice, VfioError};
-pub use vfio_pci::{VfioPciDevice, VfioPciError};
-
-// Returns a `Vec<T>` with a size in bytes at least as large as `size_in_bytes`.
+/// Returns a `Vec<T>` with a size in bytes at least as large as `size_in_bytes`.
 fn vec_with_size_in_bytes<T: Default>(size_in_bytes: usize) -> Vec<T> {
     let rounded_size = (size_in_bytes + size_of::<T>() - 1) / size_of::<T>();
     let mut v = Vec::with_capacity(rounded_size);
