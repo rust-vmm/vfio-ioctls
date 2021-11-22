6ffcae803cad8de1579a79b9c33ebfd377c86a02
Liu Jiang <gerry@linux.alibaba.com>
2021-11-22 15:59:54 +0800

    bindings: merge code from vfio-bindings repo
    Merge code from the vfio-bindings repository as a sub crate.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

---
+++ crates/vfio-bindings/src/bindings_v5_0_0/mod.rs
@@ -0,0 +1,9 @@
+// Copyright 2019 Intel Corporation. All Rights Reserved.
+// SPDX-License-Identifier: (BSD-3-Clause OR Apache-2.0)
+
+#![allow(clippy::all)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+pub mod vfio;
